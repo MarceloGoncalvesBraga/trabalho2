a1b1036159be0ca01f14c31c8717f05cf2282933
MarceloGoncalvesBraga <85029465+MarceloGoncalvesBraga@users.noreply.github.com>
2022-05-03 21:27:13 -0300

    Create init.py

---
+++ init.py
@@ -0,0 +1,35 @@
+
+class Bibliotecario:
+    def __init__(self, nome,):
+        self.nome = nome
+
+    def getNome(self):
+        return self.nome
+
+    #def setNome(self, nome):
+    #   self.nome = nome
+        
+    def toString(self):
+        return f'Nome  :',self.getNome()
+
+
+    def CadastrarLivro():
+        print("ok")
+    def CadastrarExemplar():
+        print("ok")
+
+class Gerente:
+    def __init__(self, nome,):
+        self.nome = nome
+
+    def getNome(self):
+        return self.nome
+
+    #def setNome(self, nome):
+    #   self.nome = nome
+        
+    def toString(self):
+        return f'Nome  :',self.getNome()
+class Funcionario:
+    def __init__(self, nome,):
+        self.nome = nome
